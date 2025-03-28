bcffaae77bab68efbfcf6f347555fb462ba31f54
CodingKitten <121097932+CodingKitten-YT@users.noreply.github.com>
2025-03-28 09:10:04 +0000

    .

--- commands/panel.js
+++ commands/panel.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 const data = new SlashCommandBuilder()
   .setName("panel")
-  .setDescription("Creates the proxy panel")
+  .setDescription("Creates the link panel")
   .setDMPermission(false)
 	.setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
 
